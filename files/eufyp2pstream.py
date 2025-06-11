c6e6eced13b8424ac031a15c0966ba45f6782a10
wiegerzuidema <50043579+wiegerzuidema@users.noreply.github.com>
2025-06-11 16:17:58 +0200

    Update eufyp2pstream.py

--- files/eufyp2pstream.py
+++ files/eufyp2pstream.py
@@ -378,7 +378,7 @@ c = Connector(run_event)
 
 async def init_websocket():
     ws: EufySecurityWebSocket = EufySecurityWebSocket(
-        "b88c4b88-eufy-security-ws",
+        "1fb5fff3-eufy-security-ws",
         sys.argv[1],
         aiohttp.ClientSession(),
         c.on_open,
